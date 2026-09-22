254ac35b23adf3d254443b5124bb67a297b44a62
enochakorede19-cyber <enochakorede19@gmail.com>
2026-09-23 00:21:53 +0100

    feat: add MY AREA font loader

---
+++ src/theme/useFonts.js
@@ -0,0 +1,13 @@
+import { useFonts as useManropeFonts, Manrope_700Bold, Manrope_800ExtraBold } from "@expo-google-fonts/manrope";
+import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold } from "@expo-google-fonts/inter";
+
+export default function useFonts() {
+  const [loaded] = useManropeFonts({
+    Manrope_700Bold,
+    Manrope_800ExtraBold,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+  });
+  return loaded;
+}
